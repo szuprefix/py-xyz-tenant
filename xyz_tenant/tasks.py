69b7f88f31577c1072c1305654bb65064e2f1cd5
denishuang <szuprefix@126.com>
2020-02-03 20:32:00 +0800

    celery

--- xyz_tenant/tasks.py
+++ xyz_tenant/tasks.py
@@ -2,7 +2,6 @@
 # author = 'denishuang'
 from __future__ import unicode_literals
 from celery import shared_task
-from . import models
 
 from celery.utils.log import get_task_logger
 
@@ -10,6 +9,7 @@ log = get_task_logger(__name__)
 
 @shared_task(bind=True, time_limit=600)
 def gen_tenant(self, data):
-    models.Tenant(**data).save()
+    from .models import Tenant
+    Tenant(**data).save()
     return "success"
 
